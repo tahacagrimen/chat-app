056201f713e1cd8cde6164f1d7f0b5f0381826e0
tahacagrimen <tahacagrimen2@gmail.com>
2022-09-14 21:59:31 +0300

    1

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Server started on port 5000");
+});
