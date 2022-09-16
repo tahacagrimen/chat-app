bdefb9919e7dccd47fe225e3f374eaa881129d2e
tahacagrimen <tahacagrimen2@gmail.com>
2022-09-16 12:58:27 +0300

    login api added

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -27,6 +27,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      isAdmin: user.isAdmin,
       pic: user.pic,
       token: generateToken(user._id),
     });
@@ -36,26 +37,22 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-
 const authUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    const user = await User.findOne({ email });
-
-    if (user && ()) {
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            pic: user.pic,
-            token: generateToken(user._id),
-        });
-    } else {
-        res.status(401);
-        throw new Error("Invalid email or password");
-    }
-
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
-
-module.exports = { registerUser };
+module.exports = { registerUser, authUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
-const { registerUser } = require("../controllers/userControllers");
+const { registerUser, authUser } = require("../controllers/userControllers");
 
 router.route("/").post(registerUser);
-router.route("/login", authUser);
+router.route("/login").post(authUser);
 
 module.exports = router;
