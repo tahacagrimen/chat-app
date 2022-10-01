d2ba3ece25000698b756e4440ed316cf8115c199
tahacagrimen <tahacagrimen2@gmail.com>
2022-10-02 01:49:25 +0300

    1

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,12 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-const __dirname1 = path.resolve();
+const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname + "/client/build/index.html"))
   );
 } else {
   app.get("/", (req, res) => {
