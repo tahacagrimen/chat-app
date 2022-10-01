f75ee7883a545812177cbeecd122a9122efd406f
tahacagrimen <tahacagrimen2@gmail.com>
2022-10-02 01:37:05 +0300

    last touch

--- backend/server.js
+++ backend/server.js
@@ -18,16 +18,16 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-const _dirname1 = path.resolve();
+const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname1, "/client/build")));
+  app.use(express.static(path.join(__dirname1, "/client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname1, "client", "build", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
 } else {
   app.get("/", (req, res) => {
-    res.send("API is running...");
+    res.send("API is running..");
   });
 }
 
