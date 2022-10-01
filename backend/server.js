664436f2c3a5867ea72fc64793cb2bacc6df3be5
tahacagrimen <tahacagrimen2@gmail.com>
2022-10-01 22:41:41 +0300

    make it better

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const app = express();
 dotenv.config();
 const connectDB = require("./config/db");
 connectDB();
+const path = require("path");
 
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
@@ -13,20 +14,29 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
+const _dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname1, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname1, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-const server = app.listen(port, () => {
+const server = app.listen(PORT, () => {
   console.log("Server started on port 5000");
 });
 
--- client/src/components/miscellaneous/SingleChat.jsx
+++ client/src/components/miscellaneous/SingleChat.jsx
@@ -8,7 +8,7 @@ import { useEffect } from "react";
 import ScrollableMessages from "./ScrollableMessages";
 import { io } from "socket.io-client";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://mern-chat-app-taha.herokuapp.com/";
 let socket, selectedChatCompare;
 
 function SingleChat({ fetchAgain, setFetchAgain }) {
