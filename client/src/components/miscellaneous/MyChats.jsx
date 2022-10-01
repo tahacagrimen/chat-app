d4066526f43a8e9720b69f098794292c70e30f16
tahacagrimen <tahacagrimen2@gmail.com>
2022-10-02 02:08:41 +0300

    async error

--- client/src/components/miscellaneous/MyChats.jsx
+++ client/src/components/miscellaneous/MyChats.jsx
@@ -36,7 +36,7 @@ function MyChats({ fetchAgain }) {
     fetchChats();
   }, [fetchAgain]);
 
-  const getSender = (loggedUser, users) => {
+  const getSender = async (loggedUser, users) => {
     return users[0]._id === loggedUser._id ? users[1].name : users[0].name;
   };
 
