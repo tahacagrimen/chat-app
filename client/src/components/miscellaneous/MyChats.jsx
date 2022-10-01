e2d1032c212469a181d42b031a9ec0727bb04d9f
tahacagrimen <tahacagrimen2@gmail.com>
2022-10-02 02:10:53 +0300

    1

--- client/src/components/miscellaneous/MyChats.jsx
+++ client/src/components/miscellaneous/MyChats.jsx
@@ -36,7 +36,7 @@ function MyChats({ fetchAgain }) {
     fetchChats();
   }, [fetchAgain]);
 
-  const getSender = async (loggedUser, users) => {
+  const getSender = (loggedUser, users) => {
     return users[0]._id === loggedUser._id ? users[1].name : users[0].name;
   };
 
