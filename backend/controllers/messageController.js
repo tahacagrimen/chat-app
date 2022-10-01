45c96e6fbbedb6a617147726a08ec6549351916b
tahacagrimen <tahacagrimen2@gmail.com>
2022-10-01 23:52:07 +0300

    1

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
-const Chat = require("../Models/chatModel");
-const Message = require("../Models/messageModel");
-const User = require("../Models/userModel");
+const Chat = require("../models/chatModel");
+const Message = require("../models/messageModel");
+const User = require("../models/userModel");
 
 const sendMessage = asyncHandler(async (req, res) => {
   const { content, chatId } = req.body;
